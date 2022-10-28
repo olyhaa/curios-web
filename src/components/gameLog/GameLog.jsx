b9d92a194852a0e6f8fac02bbc553058f2a0e8c3
Amanda Olyha <amanda.olyha@gmail.com>
2022-10-28 14:46:38 -0400

    auto scroll log

--- src/components/gameLog/GameLog.jsx
+++ src/components/gameLog/GameLog.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { map } from 'ramda';
 import './gameLog.css';
+import { useRef, useEffect } from 'react';
 
 const GameLog = ({ log }) => {
   let count = 0;
+  const lastItemElem = useRef();
+
+  const scrollToBottom = () => {
+    lastItemElem.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  useEffect(scrollToBottom, [log]);
+
   return (
     <div className="log-container">
       {map((logItem, index) => {
         return <div key={++count}>{logItem}</div>;
       }, log)}
+      <div className="lastItem" ref={lastItemElem} />
     </div>
   );
 };
