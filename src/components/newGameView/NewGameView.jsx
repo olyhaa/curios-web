e86da7e52c45afd8ecb59e090a621a2003c475bd
Amanda Olyha <amanda.olyha@gmail.com>
2023-04-20 13:40:09 -0400

    styling for launch page

--- src/components/newGameView/NewGameView.jsx
+++ src/components/newGameView/NewGameView.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './newGameView.css';
 
 const NewGameView = ({ handleNewGame }) => {
-  return <button onClick={handleNewGame}>Start Game!</button>;
+  return (
+    <div>
+      <h1 class="title">Curios</h1>
+      <button class="new-game" onClick={handleNewGame}>Start Game!</button>
+    </div>
+  );
 };
 
 NewGameView.propTypes = {
